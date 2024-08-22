2a5e8f66051502fa9b0712b5215f2d5693aa894e
Reginaldo-Dev <137166839+Reginaldo-Dev@users.noreply.github.com>
2024-08-22 18:01:37 -0300

    Criando a dependencia FASTIFY e trabalhando a rota GET

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-import {createServer} from 'node:http'
+// import {createServer} from 'node:http'
 
-const server = createServer((req, res) => {
-   res.write('seja bem vindo')
+// const server = createServer((req, res) => {
+//    res.write('seja bem vindo')
 
-   return res.end()
+//    return res.end()
+// })
+
+// server.listen(3333)
+
+import {fastify} from 'fastify'
+
+const server = fastify()
+
+server.get('/', () => {
+   return 'Sejam Bem Vindos'
 })
 
-server.listen(3333)
