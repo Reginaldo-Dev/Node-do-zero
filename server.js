dc739a5dd5ee393afafb9c833978c3400df129bf
Reginaldo-Dev <137166839+Reginaldo-Dev@users.noreply.github.com>
2024-08-25 16:55:00 -0300

    Criação de API finalizada com sucesso

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const server = fastify()
 
 const database = new DatabaseMemory()
 
-// Request Body => enviando o corpo de uma requisição
-
 server.post('/videos', (request, reply) => {
    const {title, description, duration, } = request.body
    
@@ -19,11 +17,12 @@ server.post('/videos', (request, reply) => {
    return reply.status(201).send()
 })
 
-server.get('/videos', () => {
-   const videos = database.list()
-
-   console.log(videos)
+server.get('/videos', (requery) => {
    
+   const search = requery.query.search
+
+   const videos = database.list(search)
+     
    return videos
 })
 
